617f3825e8fe1f08a4114bf5acce6b800ae9333e
ABDOU SOULEYMANE <abdou.souleymane@ninetec.ne>
2024-05-27 18:38:50 +0100

    injection de dependance et mise à jour de la base de données

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552ea40e5df0dee225a9e9af33591cc5a5d79157")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
